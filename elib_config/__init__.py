bd50ebc61ffa3e9513ed0c9425f1fb41c6693d82
etcher <github@daribouca.net>
2018-08-24 15:24:41 +0200

    expose write_example_config_file

--- elib_config/__init__.py
+++ elib_config/__init__.py
@@ -15,19 +15,21 @@ from elib_config._file._exc import (
 from elib_config._value._exc import (
     ConfigMissingValueError, ConfigTypeError, ConfigValueError, NotAFileError, NotAFolderError, PathMustExistError,
 )
+# noinspection PyProtectedMember
+from ._file._config_example import write_example_config
 from ._setup import ELIBConfig
 # noinspection PyProtectedMember
 from ._value._config_value import SENTINEL
 # noinspection PyProtectedMember
 from ._value._config_value_bool import ConfigValueBool
 # noinspection PyProtectedMember
+from ._value._config_value_integer import ConfigValueInteger
+# noinspection PyProtectedMember
 from ._value._config_value_list import ConfigValueList
 # noinspection PyProtectedMember
 from ._value._config_value_path import ConfigValuePath
 # noinspection PyProtectedMember
 from ._value._config_value_string import ConfigValueString
-# noinspection PyProtectedMember
-from ._value._config_value_integer import ConfigValueInteger
 
 try:
     __version__ = get_distribution('elib_config').version
