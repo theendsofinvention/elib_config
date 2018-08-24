93b390598fc724d48d41c29d7b080047b5834a40
etcher <github@daribouca.net>
2018-08-24 09:05:14 +0200

    add dev reqs to setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,22 @@ requirements = [
     'toml',
     'setuptools_scm',
 ]
-test_requirements = []
+test_requirements = [
+    'pytest',
+    'pytest-pycharm',
+    'hypothesis',
+    'pytest-cache',
+    'coverage',
+    'wheel',
+    'twine',
+    'autopep8',
+    'pylint',
+    'safety',
+    'flake8',
+    'pytest-cov',
+    'mypy',
+    'mockito',
+]
 
 CLASSIFIERS = filter(None, map(str.strip,
                                """
