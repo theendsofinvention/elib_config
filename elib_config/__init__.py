8a7702e5c6322b5d0bf4ed9e4cc1284ccc5cc05a
etcher <github@daribouca.net>
2018-08-24 06:41:47 +0200

    expose package API

--- elib_config/__init__.py
+++ elib_config/__init__.py
@@ -7,7 +7,13 @@ It is intended for my personal use only
 
 from pkg_resources import DistributionNotFound, get_distribution
 
-# noinspection PyUnresolvedReferences
+from ._config_value_bool import ConfigValueBool
+from ._config_value_path import ConfigValuePath
+from ._config_value_string import ConfigValueString
+from ._exc import (
+    ConfigFileNotFoundError, ConfigMissingValueError, ConfigTypeError, ConfigValueError, ELIBConfigError,
+    EmptyValueError, IncompleteSetupError, InvalidConfigFileError, NotAFileError, NotAFolderError, PathMustExistError,
+)
 from ._setup import ELIBConfig
 
 try:
