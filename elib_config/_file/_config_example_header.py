a041b6a0e7a7a9c87dcdaabea149d3052e06aa06
etcher <github@daribouca.net>
2018-08-27 10:24:16 +0200

    update header for example file to reflect TOML

--- elib_config/_file/_config_example_header.py
+++ elib_config/_file/_config_example_header.py
@@ -14,7 +14,7 @@ name!
 
 It must be renamed to "{config_file_path}" in order to come into effect.
 
-The configuration file follows the YAML format.
+The configuration file follows the TOML format specification.
 
 Mandatory and optional values
 =============================
@@ -22,39 +22,8 @@ Mandatory and optional values
 Some values are mandatory, some aren't. Mandatory values are marked with the "MANDATORY VALUE" tag. If mandatory values
 are missing, {app_name} will not start.
 
-Note that, while not necessary for {app_name} to function, failing to provide non-mandatory may prevent non-essential 
-modules to start. This will be indicated as such in the log and in the console.
-
-Assigning values to keys
-========================
-
-A key is a name followed by a colon. For example: "key:". The value is what follows the colon. For example, here's
-how to assign the value "some_value" to the key "some_key:
-
-    some_key: some_value
-    
-Indentation
-===========
-
-Indentation matters in YAML. Keys are grouped under a common parent. For example, a set of keys "key1", "key2" and 
-"key3" belonging to a group "Group", with their dummy values, would look like:
-
-group:
-  key1: dummy
-  key2: dummy
-  key3: dummy
-  
-Flat keys
-=========
-  
-In addition to this grouping format, keys and values can also be expressed in a flat manner. This example would result
-in the same configuration as the one above:
-
-group{sep}key1: dummy
-group{sep}key2: dummy
-group{sep}key3: dummy
-
-The different levels are separated with: "{sep}". 
+Note that, while not necessary for {app_name} to function, failing to provide non-mandatory values may prevent 
+non-essential modules to start.
 
 OS environment
 ==============
@@ -63,88 +32,53 @@ All configuration values may also be given using the OS environment instead of t
 the "{app_name}_" prefix must be added (this is to prevent any potential conflict with other application.
 
 For example, the configuration "dummy_key: dummy_value" would be set in the OS environment using the variable
-"setx {app_name}_DUMMY_KEY=dummy_value".
-
-In the case of grouped key, a key that is expressed like this in YAML:
-
-group1:
-  group2:
-    group3:
-       dummy_key: dummy_value
-       
-would be translated into an OS environment value like this:
-"setx {app_name}_GROUP1{sep}GROUP2{sep}GROUP3{sep}DUMMY_KEY=dummy_value"
-
-Please not that all configuration key given using OS environment should be UPPER CASE.
+"{app_name}_DUMMY_KEY=dummy_value".
 
 Values types
 =============
+
 Value types will be checked at runtime.
 
-Possible values are: 
-    - string
-        A string is simple text. To protect special characters such as numbers or newlines, use single quotes.
-        example_key_1: this is a valid string
-        example_key_2: 'this is also a valid string'
-        example_key_3: 10 # this is NOT a string and will result in an error
-        example_key_4: '10' # this is the correct way to use an integer as a string
-        example_key_5: '\n' # this is the correct way to have a newline character as a string
-        
-        Multiline strings are simple to write. If you want to convert newlines into spaces, you can use the ">" marker.
-        For example:
-        example_key: >
-          This is
-          not
-          a multi
-          line
-          string
-          
-        will result in "This is not a multi line string".
-        
-        If you want to preserve the new lines, use the "|" character:
-        example_key: |
-          This is
-          a true
-          multi line
-          string
-          
-    - integer
-        An integer is a number without decimals.
-        
-        example_integer_1: 1
-        example_integer_1: -1
-        example_integer_1: 1000
-    
-    - float
-        A float is a number with decimals.
-        
-        example_float_1: 1.1
-        example_float_2: 1.12345
-        example_float_3: -1.12345
-        
-    - boolean
-        A boolean is either "true" or "false".
-        
-        For example:
-        dogs_are_awesome: true
-        enough_boosters: false
-        
-        Note: despite the YAML specification, the only values accepted by {app_name} are "true" or "false". This is to 
-        prevent type casting from confusing the end-user, when any non-empty string or positive integer would yield 
-        "true".
-        
-    - list
-        A list in YAML is expressed as multiple lines beginning with a dash. For example, a list of strings:
-        strings:
-          - string1
-          - string2
-          - 'string3'
-          
-        A list of integers:
-        integers:
-          - 1
-          - 2
-          - 3
-          
-        Note the indentation for each line under the main key.
+TOML specification
+==================
+
+The full TOML specification can be found at: 
+
+Here's an example taken from the readme:
+
+####################################################################
+# This is a TOML document.
+
+title = "TOML Example"
+
+[owner]
+name = "Tom Preston-Werner"
+dob = 1979-05-27T07:32:00-08:00 # First class dates
+
+[database]
+server = "192.168.1.1"
+ports = [ 8001, 8001, 8002 ]
+connection_max = 5000
+enabled = true
+
+[servers]
+
+  # Indentation (tabs and/or spaces) is allowed but not required
+  [servers.alpha]
+  ip = "10.0.0.1"
+  dc = "eqdc10"
+
+  [servers.beta]
+  ip = "10.0.0.2"
+  dc = "eqdc10"
+
+[clients]
+data = [ ["gamma", "delta"], [1, 2] ]
+
+# Line breaks are OK when inside arrays
+hosts = [
+  "alpha",
+  "omega"
+]
+####################################################################
 '''
